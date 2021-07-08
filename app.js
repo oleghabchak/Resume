c15f29379b2bf0f214dc0afa5f4e9e388e7f6db3
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-07-08 13:44:38 +0300

    create simple count

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 "use strict";
 
+const numberOfFilms = +prompt('Скільки фільмів ви подивилися?', '');
+
+const personalsMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const   a = prompt('Останній фільм як дивилися?', ''),
+        b = prompt('Оцініть його?', ''),
+        c = prompt('Останній фільм як дивилися?', ''),
+        d = prompt('Оцініть його?', '');
+        
+personalsMovieDB.movies[a] = b;
+personalsMovieDB.movies[c] = d;
+
+console.log(personalsMovieDB);
