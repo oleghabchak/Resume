1ea9bf58214a406ed9d8a54b8966c54e2a77590a
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-07-21 17:41:33 +0300

    sldkfj

--- app.js
+++ app.js
@@ -1,38 +1,17 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const arr =[2,465,4,0,7,8];
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// arr.pop();
+// console.log(arr);
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
 
+// for ( let val of arr) {
+//     console.log(val);
+// }
 
-for (let i = 0; i<2; i++ )  {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
-    
-        if (a !=null && b !=null && a !='' && b !='' && a.length <10) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    
-}
-
-if (personalMovieDB.count < 10) {
-    console.log('мало фільмів');
-} else if ( personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('середній глядач');
-} else if (personalMovieDB.count >= 30) {
-    console.log('кіноман');
-} else {
-    console.log('помилка');
-}
-
-console.log(personalMovieDB);
+arr.forEach(function(it, i, arr) {
+    console.log(`${i}: ${it} всередині ${arr}`);
+});
