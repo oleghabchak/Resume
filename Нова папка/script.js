f99c8c4f21d0567cae757f00f9957968caa5f8c5
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-09-27 17:08:02 +0300

    commit

---
+++ Нова папка/script.js
@@ -0,0 +1,19 @@
+function User (name, age) {
+    this.age = age;
+    this.name = name;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    };
+}
+
+User.prototype.exit = function () {
+    console.log(`Користувач ${this.name}, ${this.age} вийшов`);
+}
+
+const alex = new User ('oleg', 24);
+
+alex.exit();
+
+alex.hello();
+
+console.log(alex);
