e2c255113f1ac668ee86879581f9f7430da6a060
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-10-01 00:52:45 +0300

    Update script.js

--- Нова папка/script.js
+++ Нова папка/script.js
@@ -1,10 +1,36 @@
-// function User (name, age) {
-//     this.age = age;
-//     this.name = name;
-//     this.hello = function() {
-//         console.log(`Hello ${this.name}`);
-//     };
+// function showThis(a, b){
+//     console.log(this);
+//     function sum() {
+//         return a + b;
+//     }
+
+//     console.log(sum());
 // }
+// showThis(4, 5);
+
+// const obj = {
+//     a: 20,
+//     b: 45,
+//     sum: function () {
+//         function shout() {
+//         console.log(this);
+//     }
+//     shout();
+//     }
+// };
+// obj.sum();
+// 1---звичайна фунція this= window, якщо строгий режим, то undefined
+// 2---К
+
+
+function User (name, age) {
+    this.age = age;
+    this.name = name;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    };
+}
+let ivan = new User ('ivan',23);
 
 // User.prototype.exit = function () {
 //     console.log(`Користувач ${this.name}, ${this.age} вийшов`);
@@ -18,13 +44,3 @@
 
 // console.log(alex);
 
-
-
-
-
-
-let a = 8;
-let b = 9;
-let c;
-c = a+b;
-alert(c);
