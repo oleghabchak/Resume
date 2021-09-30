e76c7257892d88bf040ab2382a99f9b9dd1663dc
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-09-30 15:52:09 +0300

    Update script.js

--- Нова папка/script.js
+++ Нова папка/script.js
@@ -1,19 +1,30 @@
-function User (name, age) {
-    this.age = age;
-    this.name = name;
-    this.hello = function() {
-        console.log(`Hello ${this.name}`);
-    };
-}
+// function User (name, age) {
+//     this.age = age;
+//     this.name = name;
+//     this.hello = function() {
+//         console.log(`Hello ${this.name}`);
+//     };
+// }
 
-User.prototype.exit = function () {
-    console.log(`Користувач ${this.name}, ${this.age} вийшов`);
-}
+// User.prototype.exit = function () {
+//     console.log(`Користувач ${this.name}, ${this.age} вийшов`);
+// }
 
-const alex = new User ('oleg', 24);
+// const alex = new User ('oleg', 24);
 
-alex.exit();
+// alex.exit();
 
-alex.hello();
+// alex.hello();
 
-console.log(alex);
+// console.log(alex);
+
+
+
+
+
+
+let a = 8;
+let b = 9;
+let c;
+c = a+b;
+alert(c);
